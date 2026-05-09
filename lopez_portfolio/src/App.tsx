09d30adab24d0ba9c9c868ea6b34133064c4ef38
Dante Lopez <daorlope@ucsc.edu>
2026-05-09 16:36:03 -0700

    Implemented nav bar and welcome page

--- lopez_portfolio/src/App.tsx
+++ lopez_portfolio/src/App.tsx
@@ -2,20 +2,25 @@ import './App.css'
 
 function Menu() {
   return (
-    <ul className="flex gap-8 px-10 m-0 p-4 menu-item">
-      <li className="hover:text-gray-500 transition-colors menu-item">Home</li>
-      <li className="hover:text-gray-500 transition-colors menu-item">Projects</li>
-      <li className="hover:text-gray-500 transition-colors menu-item">Contact</li>
+    <nav className=" bg-[#B17F59] p-4 text-[#EDE8DC] text-lg mb-0">
+      <ul className="flex list-none justify-end gap-10 px-7 m-0 menu">
+        <li className="hover:text-[#b8b3a9] transition-colors menu-item">Home</li>
+        <li className="hover:text-[#b8b3a9] transition-colors menu-item">Projects</li>
+        <li className="hover:text-[#b8b3a9] transition-colors menu-item">Contact</li>
     </ul>
+    </nav>
   );
 }
 
 function Welcome() {
   return (
-    <div>
-      <img src="./question.png" width="150" height="150"></img>
-      <h1>Dante Lopez</h1>
-      <h3>Aspiring Embedded Systems Engineer</h3>
+    <div className='flex bg-[#EDE8DC] h-[80vh] gap-7 sm:gap-15 md:gap-30 lg:gap-75 items-center justify-center'>
+      <img src="./me.jpg" width="150" height="150" className=''></img>
+      <div className="font-semibold">
+        <h1 className='text-xl sm:text-xl md:text-5xl lg:text-6xl'>Dante Lopez</h1>
+        <h3 className='text-xs sm:text-xs md:text-xs lg:text-base'>Aspiring Embedded Systems Engineer</h3>
+      </div>
+
     </div>
   );
 }
@@ -35,7 +40,7 @@ function Project() {
 
 export default function Portfolio() {
   return (
-    <section>
+    <section className=''>
       <Menu />
       <Welcome />
       <Project />
