a77ceb26974e359a8c857d092cab2d967a91e3c4
Dante Lopez <daorlope@ucsc.edu>
2026-05-09 15:14:39 -0700

    Added main portfolio content

--- lopez_portfolio/src/App.tsx
+++ lopez_portfolio/src/App.tsx
@@ -1,18 +1,46 @@
+import './App.css'
+
 function Menu() {
   return (
-    <ul>
-      <li>Home</li>
-      <li>Projects</li>
-      <li>Contact</li>
+    <ul className="flex gap-8 px-10 m-0 p-4 menu-item">
+      <li className="hover:text-gray-500 menu-item">Home</li>
+      <li className="hover:text-gray-500 menu-item">Projects</li>
+      <li className="hover:text-gray-500 menu-item">Contact</li>
     </ul>
   );
+}
+
+function Welcome() {
+  return (
+    <div>
+      <img src="../public/question.png" width="150" height="150"></img>
+      <h1>Dante Lopez</h1>
+      <h3>Aspiring Embedded Systems Engineer</h3>
+    </div>
+  );
+}
+
+function Project() {
+  return (
+    <div>
+        <img src="../public/question.png" width="150" height="150"></img>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+    </div>
+  )
 }
 
 export default function Portfolio() {
   return (
     <section>
       <Menu />
-      <Menu />
+      <Welcome />
+      <Project />
+      <Project />
+      <Project />
     </section>
   );
 }
